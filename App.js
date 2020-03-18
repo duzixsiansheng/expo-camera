b600c60cdefc8be1dca4c0006f72dcac99c1cbb2
duzixsiansheng <33381405+duzixsiansheng@users.noreply.github.com>
2020-03-17 23:56:24 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Modal, Image } from 'react-native';
 import { Camera } from 'expo-camera';
-import {FontAwesome} from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
   const camRef = useRef(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  onPictureSaved = async photo => {
+    
+    const asset = await MediaLibrary.createAssetAsync(photo.uri);
+    console.log('asset:', asset)
+  }
 
   useEffect (() => {
     (async () => {
@@ -26,9 +34,14 @@ export default function App() {
   async function takePicture(){
     if(camRef){
       const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      setOpen(true); 
       console.log(data);
     }
   }
+
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Camera
@@ -59,6 +72,26 @@ export default function App() {
           <FontAwesome name = "camera" size ={23} color='#FFF' />
       </TouchableOpacity>
       
+      { capturedPhoto &&
+        <Modal
+        animationType="slide"
+        transparent={false}
+        visible={open}
+        >
+
+          <View style = {{flex:1, justifyContent: 'center', alignItems: 'center', margin: 20}}>
+           
+           <TouchableOpacity style={{margin: 10}} onPress={() => setOpen(false)}>
+             <FontAwesome name = "window-close" size={50} color ='#FF0000'/>
+           </TouchableOpacity>
+
+           <Image
+            style ={{width:'100%', height: 300, borderRadius: 20}}
+            source={{ uri: capturedPhoto}}
+           />
+          </View>
+        </Modal>
+      }
 
 
     </SafeAreaView>
